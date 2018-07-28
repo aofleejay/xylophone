afb0f84f54804516b5510431e3d79cc8bfd21054
aofleejay <aofleejay@hotmail.com>
2018-07-29 00:30:22 +0700

    Passing in parameter instead of using global variable

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -11,7 +11,6 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer: AVAudioPlayer!
-    var selectedSoundFileName: String = ""
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
@@ -19,12 +18,11 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag - 1]
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag - 1])
     }
     
-    func playSound() {
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         // Use "try! audioPlayer = AVAudioPlayer(contentsOf: soundURL!)" instead if no error happend.
         do {
